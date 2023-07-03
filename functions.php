8838b8b6de0fe8013368d8a636037ba106779de8
sinanisler <sinan@sinanisler.com>
2023-07-03 22:59:59 +0300

    first push

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+
+add_theme_support( 'block-templates' );
+add_theme_support( 'block-template-parts' );
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header( ); ?>
+
+<? if ( have_posts() ) { 	while ( have_posts() ) { the_post();  ?>	
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php the_content( ); ?>
+</div>
+
+<? } } ?>
+
+<?php get_footer( ); ?>
